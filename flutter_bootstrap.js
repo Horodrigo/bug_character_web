83afecd8fb0f4a571a98d280453c6c7ab97a8e39
Horodrigo <hodrigocastelo@gmail.com>
2025-09-30 21:47:38 +0000

    Deploy Flutter web build: 7fdf3f57a05a80d1a1b78eabd5028b5069d421f0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3628837208"
+    serviceWorkerVersion: "4091857614"
   }
 });
