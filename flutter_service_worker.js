b9151db42981240f3e84312ee2494ebe3c6eeb6f
Horodrigo <hodrigocastelo@gmail.com>
2025-11-08 04:12:28 +0000

    Deploy Flutter web build: 5a5e4deb4a7639ac6fcfe05626f77005a7a459cc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -99,9 +99,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "caa0b97bd5607a061e8bb79c8043bede",
+"flutter_bootstrap.js": "c97059e7b441603a6f1cc4a48030ac51",
 "version.json": "402aae3e1fce99bf4a2d70a35f9601d1",
-"main.dart.js": "06a900ccc7424e6b977f11da18edd249"};
+"main.dart.js": "05f30797a7f9593a35a76faa335108f9"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
